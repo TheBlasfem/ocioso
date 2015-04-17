f882434043e46d4d67e29ab8ea66afa1e49d718c
Julio Lopez <ljuliom@gmail.com>
2015-04-17 08:44:18 -0500

    init with block

--- lib/ocioso.rb
+++ lib/ocioso.rb
@@ -1,7 +1,8 @@
 module Ocioso
-  def initialize(vars = {})
+  def initialize(vars = {}, &block)
     vars.each do |k, v|
       instance_variable_set "@#{k}", v
     end
+    yield(self) if block_given?
   end
 end
--- test/test_ocioso.rb
+++ test/test_ocioso.rb
@@ -6,8 +6,20 @@ scope do
   end
 
   test "instance variables" do
-    user = User.new name: "Julio", email: "ljuliom@gmail.com"
+    user = User.new name: "Julio", email: "julio@email.com"
     assert_equal user.instance_variable_get("@name"), "Julio"
-    assert_equal user.instance_variable_get("@email"), "ljuliom@gmail.com"
+    assert_equal user.instance_variable_get("@email"), "julio@email.com"
+  end
+
+  test "init with block" do
+    u = Class.new(User) do
+      attr_accessor :name, :email
+    end
+    u_instance = u.new do |user|
+      user.name = "Claudia"
+      user.email = "clau@email.com"
+    end
+    assert_equal u_instance.name, "Claudia"
+    assert_equal u_instance.email, "clau@email.com"
   end
 end
