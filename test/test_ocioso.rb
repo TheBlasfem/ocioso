ed03d94c6925c78f06133dfd7d2b1ffe453ac94a
Julio Lopez <ljuliom@gmail.com>
2015-04-17 08:52:50 -0500

    more tests

--- test/test_ocioso.rb
+++ test/test_ocioso.rb
@@ -5,6 +5,12 @@ scope do
     include Ocioso
   end
 
+  test "init blank" do
+    user = User.new
+    assert_equal user.is_a?(User), true
+    assert_equal user.instance_variable_get("@name"), nil
+  end
+
   test "instance variables" do
     user = User.new name: "Julio", email: "julio@email.com"
     assert_equal user.instance_variable_get("@name"), "Julio"
@@ -21,5 +27,16 @@ scope do
     end
     assert_equal u_instance.name, "Claudia"
     assert_equal u_instance.email, "clau@email.com"
+  end
+
+  test "init mix, sending hash and block" do
+    u = Class.new(User) do
+      attr_accessor :name, :email
+    end
+    u_instance = u.new name: "Piero" do |user|
+      user.email = "piero@email.com"
+    end
+    assert_equal u_instance.name, "Piero"
+    assert_equal u_instance.email, "piero@email.com"
   end
 end
